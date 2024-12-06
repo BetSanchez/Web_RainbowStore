50a6ecd51918a43ce9e494a2b55f53f41b00230d
BetSanchez <fatyrs16@gmail.com>
2024-12-05 20:26:33 -0600

    Se agrega la pagina

---
+++ src/data.js
@@ -0,0 +1,77 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+import img9 from './img/img9.jpg';
+
+
+export const data = [
+	{
+		id: 1,
+		img: img1,
+		nameProduct: 'Capibara',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: img2,
+		nameProduct: 'Kirby',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: img3,
+		nameProduct: 'Borreguito',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: img4,
+		nameProduct: 'Vaquita',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: img5,
+		nameProduct: 'Cinnamonroll',
+		price: 20,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		img: img6,
+		nameProduct: 'Osito',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 7,
+		img: img7,
+		nameProduct: 'Panda rojo',
+		price: 30,
+		quantity: 1,
+	},
+	{
+		id: 8,
+		img: img8,
+		nameProduct: 'Snoopy',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 9,
+		img: img9,
+		nameProduct: 'Champiñon',
+		price: 30,
+		quantity: 1,
+	},
+	
+];
